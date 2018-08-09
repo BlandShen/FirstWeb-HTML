f1ca17b6a2a5d56ff36b81ef645d23573993e237
BlandShen <1600430050@qq.com>
2018-08-09 11:36:07 +0800

    18.8.9 commit1

---
+++ vuecli/vuecli-demo/src/router.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Connect from './views/Connect.vue'
+import ConnectCom1 from '@/components/ConnectCom1.vue'
+import ConnectCom2 from '@/components/ConnectCom2.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      },
+      {
+          path: '/connect',
+          name: 'connect',
+          component: Connect,
+          children:[
+              {
+                  path: 'test1',
+                  name: 'test1',
+                  component: ConnectCom1
+              },
+              {
+                  path: 'test2/:name/:age',
+                  name: 'test2',
+                  component: ConnectCom2
+              },
+              {
+                  path:'backtest2/:name/:age',
+                  redirect: 'test2/:name/:age'
+              }
+          ]
+      },
+      {
+          path: '/backhome',
+          // name: 'backhome',
+          redirect:"/"
+      },
+      {
+          path:'/aliashome',
+          component:Home,
+          alias:"/homealias"
+      }
+  ]
+})
---
+++ vuecli/vuecli-demo/src/views/Connect.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>联系我们</h1>
+        <router-link :to="{name:'test1',params:{name:'lisi',age:20}}">test1</router-link> |
+        <router-link to="/connect/test2/zhangsan/30">test2</router-link> |
+        <router-link to="/connect/backtest2/zhangsan/30">backtest2</router-link>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+
+
+
+
+</script>
+
+<style scoped>
+
+
+
+</style>
